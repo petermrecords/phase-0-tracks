cb254f1fb39d8b9fc9a385d7c8d3d4cc788d5711
PeterMRecords <petermennitirecords@gmail.com>
2017-01-04 21:00:09 -0500

    release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts 'What is your name?'
+your_name = gets.chomp
+puts 'How old are you?'
+your_age = gets.chomp
+puts 'Our company cafeteria serves garlic bread.  Should we order some for you?'
+garlic_bread = gets.chomp
+puts 'Would you like to enroll in the company\'s health insurance?'
+health_insurance = gets.chomp
+
+puts your_name
+puts your_age
+puts garlic_bread
+puts health_insurance
