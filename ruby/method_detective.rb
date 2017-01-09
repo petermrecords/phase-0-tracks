b84869dbbceeb9d7ff00ab486dc1b64348419642
PeterMRecords <petermennitirecords@gmail.com>
2017-01-08 20:10:30 -0500

    everything

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,46 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, 'o')
+"zom".sub('o', 'oo')
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(' suspects')
+"the usual" << ' suspects'
+"the usual" += ' suspects'
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend('the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chomp('r')
+"The case of the disappearing last letter".slice(0,39)
+"The case of the disappearing last letter".sub('er','e')
+"The case of the disappearing last letter".tr_s('er','e')
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1,40)
+"The mystery of the missing first letter".delete('T')
+"The mystery of the missing first letter".sub('T','')
+"The mystery of the missing first letter".tr('T','')
+
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(' ')
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count('a')
 # => 4
