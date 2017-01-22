386e63511d15f9b7f8a1dfb620833cac65d725b7
PeterMRecords <petermennitirecords@gmail.com>
2017-01-22 11:29:18 -0500

    fixed initialization

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -2,8 +2,8 @@ class Game
 	attr_reader :hidden_word, :player_word, :guessed_letters, :rounds_played, :max_rounds, :won
 
 	def initialize(word)
-		@hidden_word = word.split
-		@player_word = ("_"*hidden_word.length).split
+		@hidden_word = word.chars
+		@player_word = ("_"*hidden_word.length).chars
 		@guessed_letters = []
 		@rounds_played = 0
 		@max_rounds = (hidden_word.length*2) <= 26 ? hidden_word.length*2 : 26
