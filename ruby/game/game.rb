a4ab14143ac34213f2899eb99c76dae76204e07e
PeterMRecords <petermennitirecords@gmail.com>
2017-01-22 11:38:00 -0500

    finished guessing

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -9,4 +9,21 @@ class Game
 		@max_rounds = (hidden_word.length*2) <= 26 ? hidden_word.length*2 : 26
 		@won = nil
 	end
+
+	def guess(letter)
+		if @guessed_letters.include?(letter)
+			puts 'You already guessed that one!'
+		else
+			if @hidden_word.include?(letter)
+				@player_word.length.times do |time|
+					player_word[time] = letter if @hidden_word[time] == letter
+				end
+			else
+				puts 'Wrong!'
+			end
+			@guessed_letters << letter
+			@rounds_played += 1
+		end
+		@player_word.join('')
+	end
 end
