02211e1879d95294ef530b360ad3451907609430
PeterMRecords <petermennitirecords@gmail.com>
2017-02-02 21:08:37 -0500

    release 0

---
+++ databases/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE players(
+id INTEGER PRIMARY KEY,
+name VARCHAR(255),
+number INTEGER,
+team VARCHAR(255)
+);
+
+INSERT INTO players (name, number, team) VALUES ('Paula Bunion', 45, 'Wrecking Belles');
+INSERT INTO players (name, number, team) VALUES ('Grave Danger', 6, 'Team Unicorn');
