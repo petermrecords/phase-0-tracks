7de73198022870a6967e92d5da0f19e1454cc85a
PeterMRecords <petermennitirecords@gmail.com>
2017-02-02 22:21:23 -0500

    finished assignment

--- databases/create.sql
+++ databases/create.sql
@@ -1,3 +1,6 @@
+
+--Create players table
+
 CREATE TABLE players(
 id INTEGER PRIMARY KEY,
 name VARCHAR(255),
@@ -7,3 +10,35 @@ team VARCHAR(255)
 
 INSERT INTO players (name, number, team) VALUES ('Paula Bunion', 45, 'Wrecking Belles');
 INSERT INTO players (name, number, team) VALUES ('Grave Danger', 6, 'Team Unicorn');
+
+--Create teams table
+
+CREATE TABLE teams(
+id INTEGER PRIMARY KEY,
+name VARCHAR(255)
+);
+
+INSERT INTO teams (name) VALUES ('Wrecking Belles');
+INSERT INTO teams (name) VALUES ('Team Unicorn');
+
+--Drop & recreate players w/ foreign keys
+
+DROP TABLE players;
+
+CREATE TABLE players(
+id INTEGER PRIMARY KEY,
+name VARCHAR(255),
+number INTEGER,
+team_id INTEGER,
+FOREIGN KEY(team_id) REFERENCES teams(id)
+);
+
+INSERT INTO players (name, number, team_id) VALUES ('Paula Bunion', 45, 1);
+INSERT INTO players (name, number, team_id) VALUES ('Grave Danger', 6, 2);
+INSERT INTO players (name, number, team_id) VALUES ('April Bloodgate', 12, 2);
+
+--Queries
+
+SELECT * FROM players INNER JOIN teams ON players.team_id = teams.id;
+
+SELECT players.name, players.number, teams.name FROM players INNER JOIN teams on players.team_id = teams.id;
