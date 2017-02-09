16f45ab8ccdc6809b7755f23407963373436077e
PeterMRecords <petermennitirecords@gmail.com>
2017-02-09 18:15:02 -0500

    release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,12 +1,19 @@
-img = document.getElementsByTagName('img')
-p = document.getElementByID('one')
-img_button = document.getElementByID('img-button')
-txt_button = document.getElementByID('txt-button')
+var img = document.getElementsByTagName('img');
+var p = document.getElementById('one');
+var img_button = document.getElementById('img-button');
+var txt_button = document.getElementById('txt-button');
 
 function img_click (event) {
 	console.log("Click detected.");
 	console.log(event);
-	event.target.style.border = "2px dashed blue";
+	img[0].style.border = "2px dashed blue";
+}
+
+function txt_click (event) {
+	console.log("Click detected.");
+	console.log(event);
+	p.outerText = "I have been manipulated by Javascript."
 }
 
 img_button.addEventListener("click", img_click);
+txt_button.addEventListener("click", txt_click);
